77755a9d7c20113f42db95a3b63715cba2ed5dbc
marysol <marilirulita@gmail.com>
2021-06-05 01:20:43 -0700

    Add style to nav bar app

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :friends
   #get 'home/index'
   get 'home/about'
-  root 'home#index'
+  #root 'home#index'
+  root 'friends#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
